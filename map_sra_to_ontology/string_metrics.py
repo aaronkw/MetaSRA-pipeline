1703262b32dbb66663249ae0f3118bf74b12894b
ygliu2016 <yanbin_liu@yahoo.com>
2022-09-08 17:12:56 -0400

    reimplement to reduce from 147 to 80 secs for 10K data

--- map_sra_to_ontology/string_metrics.py
+++ map_sra_to_ontology/string_metrics.py
@@ -1,6 +1,6 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
-def bag_dist_multiset(str_a, str_b):
+def bag_dist_multiset_sav (str_a, str_b):
     count_a = Counter(str_a)
     count_b = Counter(str_b)
 
@@ -25,3 +25,23 @@ def bag_dist_multiset(str_a, str_b):
     else:
         return b_minus_a
 
+def bag_dist_multiset (str_a, str_b):
+    count = defaultdict(int)
+    for s in str_a:
+        count[s] += 1
+    for s in str_b:
+        count[s] -=1
+    pos, neg = 0, 0
+    for v in count.values():
+        if v > 0:
+           pos += v
+        else:
+           neg += v
+    neg = -neg
+    #tot = sum (count.values())
+    #pos = sum ([c  for c in count.values() if c > 0])
+    #neg = sum ([-c for c in count.values() if c < 0])
+    #neg = pos - tot        # (-neg) + pos = total
+    return pos if pos > neg else neg
+        
+        
