7fe6446b5be6aed75ad281a1bd523a6efb92a141
ygliu2016 <yanbin_liu@yahoo.com>
2023-01-10 11:57:24 -0500

    address comment

--- disbatch/disB_run.sh
+++ disbatch/disB_run.sh
@@ -3,16 +3,18 @@
 id=$1
 shift
 
+SRC_DIR=~/projects/MetaSRA-pipeline/           # the directory of run_pipeline.py
+LOG_DIR=$SRC_DIR/disbatch/log                  # the directory of log files
+VENV_DIR=$SRC_DIR/env_python38/                # the directory of virtual environment. We use python virtual environment here.
+                                               # you can use a different virutal environment and should change directory and command accordingly
+mkdir -p "$LOG_DIR"
+
 module purge
 module load python3
 
-DIR_LOG=./log
-mkdir -p "$DIR_LOG"
+#activate the python virtual environment created beforehand
+source $VENV_DIR/bin/activate
 
-#go to the project source directory 
-cd ~/projects/MetaSRA-pipeline/
-#activate a python virtual environment we have created 
-#if you use a differnt virutal environment, you should change command accordingly
-source ./env_python38/bin/activate
-
-python run_pipeline.py $@ > $DIR_LOG/${id}.out 2> $DIR_LOG/${id}.log
+#run the command
+echo "python $SRC_DIR/run_pipeline.py $@ > $LOG_DIR/${id}.out 2> $LOG_DIR/${id}.log"
+#python $SRC_DIR/run_pipeline.py $@ > $LOG_DIR/${id}.out 2> $LOG_DIR/${id}.log
